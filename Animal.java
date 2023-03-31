e45e135e8fdccac945bca2b82dba695310d7c5aa
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-31 09:49:51 -0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -2,18 +2,31 @@ package javaPOO;
 
 public class Animal {
 	
-	/*criando os atributos*/
 	
-	public String nome;
-	public double peso;
-	
+	private String nome;
+	private double peso;
+
 	
 	public Animal() {}
-	
+	public Animal(String nome, double peso) {
+		this.nome = nome;
+		this.peso = peso;
+	}
+
 	public void Imprimir() {
 		System.out.println(
-						   "Nome: " + this.nome +
-						   "\nPeso: " + this.peso);
+				   "Nome: " + this.nome +
+				   "\nPeso: " + this.peso);
 	}
+
 	
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+	
+		public void setPeso(double peso) {
+			this.peso = peso;
+		}
+		
 }
+
