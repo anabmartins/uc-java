e9a82bdd1bea05b448d64b69b4fa5b80ff84452c
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-31 09:19:27 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package javaPOO;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		Animal bil = new Animal();
+		bil.nome = "Bil Souza";
+		bil.peso = 2.5;
+		bil.Imprimir();
+	}
+}
