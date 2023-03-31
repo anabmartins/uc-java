1abddf21ca5f7bb17e00906a1c0041e6fd60ee18
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-31 09:50:20 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args){
 		
 		Animal bil = new Animal();
-		bil.nome = "Bil Souza";
-		bil.peso = 2.5;
+		
+		bil.setNome("Bill Souza");
 		bil.Imprimir();
 	}
 }
