02aa85856ae3ba95f0fc3f24089e593e21eab54a
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-03-31 09:49:29 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,20 @@
+package javaPOO;
+
+public class Cachorro {
+	
+	public String nome;
+
+	public Cachorro(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
